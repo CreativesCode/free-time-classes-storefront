745f5beb9ade3cfca77a2a2ee2d01510119368f9
Roberto Cabrera Alvarez <robert.cabrer92@gmail.com>
2025-03-05 16:28:46 -0500

    Configure Next.js for static export and add Capacitor mobile scripts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.free.time.classes',
-  appName: 'Free Time Classes',
-  webDir: 'public'
+  appId: "com.free.time.classes",
+  appName: "Free Time Classes",
+  webDir: "out",
 };
 
 export default config;
