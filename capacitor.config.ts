98fea4583083a9f9d5719793a192282189a9f9eb
Roberto Cabrera Alvarez <robert.cabrer92@gmail.com>
2025-03-05 16:20:40 -0500

    add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.free.time.classes',
+  appName: 'Free Time Classes',
+  webDir: 'public'
+};
+
+export default config;
